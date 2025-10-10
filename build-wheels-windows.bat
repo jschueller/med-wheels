77f529f020f384187fb2de291359a7a78ceb6d98
Julien Schueller <schueller@phimeca.com>
2025-10-10 15:53:28 +0200

    Update to 4.2.0

--- build-wheels-windows.bat
+++ build-wheels-windows.bat
@@ -16,12 +16,13 @@ cmake -LAH -S hdf5 -B build_hdf5 -DCMAKE_INSTALL_PREFIX=C:/Libraries/hdf5 -DBUIL
 cmake --build build_hdf5 --config Release --target install
 
 :: med
-curl -LO https://files.salome-platform.org/Salome/other/med-%VERSION%.tar.gz
+::curl -LO https://files.salome-platform.org/Salome/medfile/med-%VERSION%.tar.gz
+curl -L -o med-%VERSION%.tar.gz https://files.catbox.moe/zm3to1.gz
 7z x med-%VERSION%.tar.gz > nul
 dir /p
 7z x med-%VERSION%.tar > nul
 dir /p
-cmake -LAH -S med-%VERSION%_SRC -B build_med -DCMAKE_INSTALL_PREFIX=C:/Libraries/med -DHDF5_ROOT_DIR=C:/Libraries/hdf5 ^
+cmake -LAH -S med-%VERSION% -B build_med -DCMAKE_INSTALL_PREFIX=C:/Libraries/med -DHDF5_ROOT_DIR=C:/Libraries/hdf5 ^
   -DMEDFILE_BUILD_TESTS=OFF -DMEDFILE_INSTALL_DOC=OFF -DMEDFILE_BUILD_PYTHON=ON ^
   -DPYTHON_LIBRARY=%PYTHON_ROOT%\libs\python%ABI:~2%.lib -DPYTHON_INCLUDE_DIR=%PYTHON_ROOT%\include ^
   -DPYTHON_EXECUTABLE=%PYTHON_ROOT%\python.exe
@@ -36,13 +37,13 @@ curl -LO https://github.com/lucasg/Dependencies/releases/download/v1.11.1/Depend
 Dependencies.exe -modules C:\Libraries\med\lib\python%PY_VER%\site-packages\med\_medenum.pyd
 
 pushd C:\Libraries\med\lib\python%PY_VER%\site-packages
-mkdir salome_med-%VERSION%.dist-info
-sed "s|@PACKAGE_VERSION@|%VERSION%|g" %GITHUB_WORKSPACE%\METADATA.in > salome_med-%VERSION%.dist-info\METADATA
-type salome_med-%VERSION%.dist-info\METADATA
-echo Wheel-Version: 1.0 > salome_med-%VERSION%.dist-info\WHEEL
-echo salome_med-%VERSION%.dist-info\RECORD,, > salome_med-%VERSION%.dist-info\RECORD
+mkdir medfile-%VERSION%.dist-info
+sed "s|@PACKAGE_VERSION@|%VERSION%|g" %GITHUB_WORKSPACE%\METADATA.in > medfile-%VERSION%.dist-info\METADATA
+type medfile-%VERSION%.dist-info\METADATA
+echo Wheel-Version: 1.0 > medfile-%VERSION%.dist-info\WHEEL
+echo medfile-%VERSION%.dist-info\RECORD,, > medfile-%VERSION%.dist-info\RECORD
 mkdir %GITHUB_WORKSPACE%\wheelhouse
-7z a -tzip %GITHUB_WORKSPACE%\wheelhouse\salome_med-%VERSION%-%ABI%-%ABI%-win_amd64.whl med salome_med-%VERSION%.dist-info
-pip install %GITHUB_WORKSPACE%\wheelhouse\salome_med-%VERSION%-%ABI%-%ABI%-win_amd64.whl
+7z a -tzip %GITHUB_WORKSPACE%\wheelhouse\medfile-%VERSION%-%ABI%-%ABI%-win_amd64.whl med medfile-%VERSION%.dist-info
+pip install %GITHUB_WORKSPACE%\wheelhouse\medfile-%VERSION%-%ABI%-%ABI%-win_amd64.whl
 pushd %GITHUB_WORKSPACE%
 python -c "import med; print(42)"
